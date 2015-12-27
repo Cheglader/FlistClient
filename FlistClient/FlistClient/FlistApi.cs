80883e1e5e78d412f33d1188b4adb38cba0a8c84
Luis San Pablo <luis.sanpablo@utexas.edu>
2015-12-26 23:49:06 -0600

    Interfacing Starting

---
+++ FlistClient/FlistClient/FlistApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Flurl;
+using Newtonsoft.Json.Linq;
+
+namespace FlistClient
+{
+    public class FlistApi
+    {
+        private class ApiRestaurant
+        {
+            public uint id;
+            public string name;
+        }
+        private class ApiCategory
+        {
+            public uint id;
+            public string name;
+        }
+        public FlistApi()
+        {
+        }
+
+        public List<Restaurant> get_restaurant_list() { return new List<Restaurant>(); }
+        public List<dynamic> get_category_list()
+        {
+            List<dynamic> ret_val = new List<dynamic>();
+            string api_json = "Finish";
+            JObject api_response = JObject.Parse(api_json);
+            JArray category_api_objects = (JArray)api_response["results"];
+            foreach(JObject item in category_api_objects.Children<JObject>())
+            {
+                ret_val.Add(item);
+            }
+            return ret_val;
+        }
+    }
+}
+
---
+++ FlistClient/FlistClient/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlistClient
+{
+    public interface ImageManager
+    {
+        List<string> get_image_strings();
+
+    }
+    public class Restaurant
+    {
+        private string name;
+        private uint id;
+        private ImageManager image_manager;
+        public Restaurant ()
+        {
+            
+        }
+    }
+}
